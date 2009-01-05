f8bed6298e489f0526cdff9d291eee037360efbb
Matthew Todd <matthew.todd@gmail.com>
2009-01-05 13:12:29 +0300

    Hide the stacktrace.

--- lib/downloads/commands/sync.rb
+++ lib/downloads/commands/sync.rb
@@ -22,7 +22,13 @@ module Downloads
         unless kill
           pid = fork { exec("rsync --recursive --progress --partial #{remote.rsync_path} #{local.rsync_path}") }
           File.open(PID_FILE, 'w') { |file| file.write(pid) }
-          Process.wait
+
+          begin
+            Process.wait
+          rescue Interrupt
+            # we don't need to see the stacktrace
+            puts # but a blank line is nice
+          end
         end
       end
     end
