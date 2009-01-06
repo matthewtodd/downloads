cbaf8de387fba5c8cba8c1d4d21aa04e38c573d2
Matthew Todd <matthew.todd@gmail.com>
2009-01-06 13:22:40 +0300

    Don't re-load yaml file each time we're asked.

--- lib/downloads/servers/remote.rb
+++ lib/downloads/servers/remote.rb
@@ -4,12 +4,16 @@ module Downloads
   module Servers
     class Remote < Base
       def initialize(host, directory, cache_path)
-        @host, @directory, @cache_path, @connection = host, directory, cache_path, nil
+        @host       = host
+        @directory  = directory
+        @cache_path = cache_path
+        @connection = nil
+        @files      = YAML.load_file(@cache_path) if File.exists?(@cache_path)
       end
 
       def files
-        update_file_cache unless File.exists?(@cache_path)
-        YAML.load_file(@cache_path)
+        populate_file_cache unless @files
+        @files
       end
 
       def rsync_path
@@ -18,28 +22,30 @@ module Downloads
 
       def run(command)
         result = run_in_directory(command)
-        update_file_cache
+        populate_file_cache
         result
       end
 
       private
 
       def connection
-        unless @connection
-          @connection = Net::SSH.start(@host, ENV['USER'])
-          at_exit { @connection.close }
-        end
-
+        initialize_connection unless @connection
         @connection
+      end
+
+      def initialize_connection
+        @connection = Net::SSH.start(@host, ENV['USER']) # TODO does it make sense to use ENV['USER']?
+        at_exit { @connection.close }
       end
 
       def run_in_directory(command)
         connection.exec!("cd #{@directory}; #{command}")
       end
 
-      def update_file_cache
+      def populate_file_cache
         yaml = run_in_directory(%{ruby -ryaml -e "puts Dir.glob('*').sort.map { |name| { :name => name, :size => File.size(name) } }.to_yaml"})
         File.open(@cache_path, 'w') { |file| file.write(yaml) }
+        @files = YAML.load(yaml)
       end
     end
   end
